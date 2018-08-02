0fcdaf162b9313f7c9fa5a665d52d207532e6d1b
xulu <xulu@fangjisuo.com>
2018-08-02 11:41:07 +0800

    init

--- src/main/java/com/xulu/pandora/PandoraApplication.java
+++ src/main/java/com/xulu/pandora/PandoraApplication.java
@@ -1,12 +1,18 @@
 package com.xulu.pandora;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.context.request.RequestContextListener;
 
 /**
  * Created by xulu on 2018/8/2.
  */
+@SpringBootApplication
+@EnableTransactionManagement
+@ComponentScan(basePackages = "com.xulu")
 public class PandoraApplication {
 
     public static void main(String[] args) {
